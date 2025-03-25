523032ac629484a4897bc003147238160e51da9a
BrunoJCosta <brunojereissati00@gmail.com>
2025-03-25 14:18:28 -0300

    refactor(cambio): improve the migrations

--- src/main/resources/db/migration/V1__Create_Table_Cambio.sql
+++ src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE cambio (
+﻿create schema if not EXISTS cambio;
+CREATE TABLE cambio.cambio (
   id bigserial PRIMARY KEY,
   from_currency varchar(3) NOT NULL,
   to_currency varchar(3) NOT NULL,
