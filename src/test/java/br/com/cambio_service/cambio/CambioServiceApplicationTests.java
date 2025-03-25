a5d1f4c48db9befcc8b49238b74fd74ce9880735
BrunoJCosta <brunojereissati00@gmail.com>
2025-03-25 14:19:34 -0300

    test(cambio): configuration and created the test of findFromAndTo

--- src/test/java/br/com/cambio_service/cambio/CambioServiceApplicationTests.java
+++ src/test/java/br/com/cambio_service/cambio/CambioServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package br.com.cambio_service.cambio;
 
+import br.com.cambio_service.cambio.service.CambioService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
