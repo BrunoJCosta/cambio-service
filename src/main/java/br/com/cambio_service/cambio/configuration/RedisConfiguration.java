96266f2620a895a5b47b42928fd1168fc212be60
BrunoJCosta <brunojereissati00@gmail.com>
2025-06-16 21:17:40 -0300

    feat(cache): configure cache on project

---
+++ src/main/java/br/com/cambio_service/cambio/configuration/RedisConfiguration.java
@@ -0,0 +1,56 @@
+package br.com.cambio_service.cambio.configuration;
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfiguration {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(6379);
+        config.setDatabase(0);
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(8); // Max active connections
+        poolConfig.setMaxIdle(8); // Max idle connections
+        poolConfig.setMinIdle(0); // Min idle connections
+        poolConfig.setMaxWait(Duration.ofSeconds(30)); // Max wait time (ms)
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    private static RedisCacheManager.RedisCacheManagerBuilder CacheManager(JedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory);
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return this::mountedCache;
+    }
+
+    private void mountedCache(RedisCacheManager.RedisCacheManagerBuilder builder) {
+        CacheName.cache().forEach(cacheName -> {
+            RedisCacheConfiguration redisDefault = RedisCacheConfiguration
+                    .defaultCacheConfig()
+                    .entryTtl(cacheName.duration())
+                    .disableCachingNullValues();
+
+            builder.withCacheConfiguration(cacheName.name(),  redisDefault);
+        });
+    }
+
+}
---
+++ src/main/java/br/com/cambio_service/cambio/configuration/RedisDTO.java
@@ -0,0 +1,6 @@
+package br.com.cambio_service.cambio.configuration;
+
+import java.time.Duration;
+
+record RedisDTO(String name, Duration duration) {
+}
