a9cc96a021a9b270d6ff620e0fd1a47adb88e8be
BrunoJCosta <brunojereissati00@gmail.com>
2025-06-17 21:07:08 -0300

    feat(cache): putting cache repository's call

--- src/main/java/br/com/cambio_service/cambio/configuration/CacheName.java
+++ src/main/java/br/com/cambio_service/cambio/configuration/CacheName.java
@@ -6,12 +6,11 @@ import java.util.List;
 public class CacheName {
 
     public static final String CAMBIO_ALL = "cambio_all";
-    public static final String CAMBIO_BY_FROM_AND_TO = "cambio_by_id";
-    private static String nome;
+    public static final String CAMBIO_BY_FROM_AND_TO = "cambio_by_from_to";
 
     static List<RedisDTO> cache() {
         return List.of(
-                new RedisDTO(CAMBIO_ALL, Duration.ofSeconds(30)),
+                new RedisDTO(CAMBIO_ALL, Duration.ofMinutes(30)),
                 new RedisDTO(CAMBIO_BY_FROM_AND_TO, Duration.ofMinutes(30))
         );
     }
