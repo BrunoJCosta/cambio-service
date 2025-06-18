a150f6a46083bb258bc2d76c7664f37168357483
BrunoJCosta <brunojereissati00@gmail.com>
2025-06-17 21:08:00 -0300

    feat(cache): reduce the pool max

--- src/main/java/br/com/cambio_service/cambio/configuration/RedisConfiguration.java
+++ src/main/java/br/com/cambio_service/cambio/configuration/RedisConfiguration.java
@@ -24,10 +24,10 @@ public class RedisConfiguration {
         config.setDatabase(0);
 
         JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(8); // Max active connections
-        poolConfig.setMaxIdle(8); // Max idle connections
-        poolConfig.setMinIdle(0); // Min idle connections
-        poolConfig.setMaxWait(Duration.ofSeconds(30)); // Max wait time (ms)
+        poolConfig.setMaxTotal(4);
+        poolConfig.setMaxIdle(4);
+        poolConfig.setMinIdle(0);
+        poolConfig.setMaxWait(Duration.ofSeconds(30));
         return new JedisConnectionFactory(config);
     }
 
