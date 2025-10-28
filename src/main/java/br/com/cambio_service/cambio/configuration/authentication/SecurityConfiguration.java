af749af4f6a556ab9ef5cd2aaabb9111569c0dfe
BrunoJCosta <brunojereissati00@gmail.com>
2025-10-27 21:43:18 -0300

    feat(cache): create the security by basic auth to request private

---
+++ src/main/java/br/com/cambio_service/cambio/configuration/authentication/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package br.com.cambio_service.cambio.configuration.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    private static final String[] GET_PUBLIC = {
+            "/cambio/currency"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   CustomFilter customFilter,
+                                                   AuthenticationEntryPoint entryPoint) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(basic -> basic.authenticationEntryPoint(entryPoint))
+                .addFilterBefore(customFilter, BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(GET_PUBLIC).permitAll()
+                                .anyRequest().authenticated())
+                .build();
+    }
+}
